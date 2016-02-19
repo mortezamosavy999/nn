d5e7a69c23cf41d212b6851e88d177d394300957
BlackHat(س.مرتضی) <www.mortezamosavy0910@gmail.com>
2016-02-19 13:25:33 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "weather",
       "xkcd",
       "youtube" },
-    sudo_users = {our_id},
+    sudo_users = {67255537,93213608,167912005,157337513},
     disabled_channels = {}
   }
   serialize_to_file(config, './data/config.lua')
